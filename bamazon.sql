20b117b0d76fecd459c9c96c3abd435efa02ce44
Rich McRary <rgmcrary@gmail.com>
2017-09-15 20:35:06 -0400

    Completed the sql file

--- bamazon.sql
+++ bamazon.sql
@@ -4,34 +4,40 @@ CREATE database bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   price DECIMAL(10,2) NULL,
   product_sales DECIMAL(10,2) NULL,
   stock_quantity INT(10) NULL,
-  PRIMARY KEY (department_name)
+  PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bicycle", "Sporting Goods", 200.00, 10),
-VALUES ("TV", "Electronics", 900.00, 15),
-VALUES ("Playstation 4", "Electronics", 400.00, 20),
-VALUES ("Football", "Sporting Goods", 20.00, 10),
-VALUES ("Xbox One", "Electronics", 400.00, 20),
-VALUES ("Leather Jacket", "Clothing", 350.00, 5),
-VALUES ("Levi's Jeans", "Clothing", 65.00, 12),
-VALUES ("Watch", "Jewelry", 375.00, 8),
-VALUES ("Diamond Ring", "Jewelry", 750.00, 3),
-VALUES ("Dr. Marten Boots", "Clothing", 300.00, 8);
+VALUES ("bicycle", "sporting goods", 200.00, 10),
+        ("tv", "electronics", 900.00, 15),
+        ("playstation 4", "electronics", 400.00, 20),
+        ("football", "sporting goods", 20.00, 10),
+        ("xbox One", "electronics", 400.00, 20),
+        ("leather jacket", "clothing", 350.00, 5),
+        ("levi's jeans", "clothing", 65.00, 12),
+        ("wWatch", "jewelry", 375.00, 8),
+        ("diamond Ring", "jewelry", 750.00, 3),
+        ("dr. marten boots", "clothing", 300.00, 8);
 
 
 CREATE TABLE departments (
-  department_id INT NOT NULL,
+  department_id INT AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(100) NULL,
   over_head_costs DECIMAL(10,2) NULL,
-  PRIMARY KEY (department_name)
+  PRIMARY KEY (department_id)
 );
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("clothing", 12000.00),
+        ("electronics", 30000.00),
+        ("jewelry", 15000.00),
+        ("sporting goods", 10000.00);
 
 SELECT * FROM products;
 select * from departments;
